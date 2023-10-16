f803cdd3da1af95c06cd3eb76f63b404c76dfa9e
ln-dev7 <leonelngoya@gmail.com>
2023-10-16 20:01:07 +0100

    resize screen

---
+++ app/home/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect } from "react";
+import gsap from "gsap";
+
+export default function Home() {
+
+  return (
+    <main className="relative flex min-h-screen flex-col items-center w-full">
+        
+        </main>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sora: ["var(--font-sora)"],
+      },
+      colors: {
+        primary: "#0A1340",
+        textmain: "#253554",
+        textmuted: "#637394",
+        danger: "#BC002E"
       },
     },
   },
